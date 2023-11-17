7e4e71144e69e1ea84aa2d58550c77eb994d8495
Nicolai <134018297+NicolaiTesla4@users.noreply.github.com>
2023-11-16 21:43:05 -0500

    add files 4

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+document.addEventListener('DOMContentLoaded', function () {
+  const player = new Player(document.getElementById('vimeo-player'));
+
+  const storageKey = 'videoplayer-current-time';
+
+  const getStoredTime = () => {
+    const storedTime = localStorage.getItem(storageKey);
+    return storedTime ? parseFloat(storedTime) : 0;
+  };
+
+  const saveCurrentTime = () => {
+    player.getCurrentTime().then(currentTime => {
+      localStorage.setItem(storageKey, currentTime);
+    });
+  };
+
+  const setPlayerTimeFromStorage = () => {
+    const storedTime = getStoredTime();
+    player.setCurrentTime(storedTime);
+  };
+
+  const throttledSaveCurrentTime = throttle(saveCurrentTime, 1000);
+
+  player.on('timeupdate', () => {
+    throttledSaveCurrentTime();
+  });
+
+  setPlayerTimeFromStorage();
+});
