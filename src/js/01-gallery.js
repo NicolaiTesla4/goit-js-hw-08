08fb6b1759ff0d9ca5991c40aab193bc6a2c6a0d
Nicolai <134018297+NicolaiTesla4@users.noreply.github.com>
2023-11-16 20:46:31 -0500

    add files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+// Importación adicional de estilos
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { galleryItems } from './gallery-items';
 
 console.log(galleryItems);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      item =>
+        `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+        <img
+        class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}"/>
+        </a>
+        </li>`
+    )
+    .join('');
+}
+
+console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+gallery.innerHTML = createGalleryMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
